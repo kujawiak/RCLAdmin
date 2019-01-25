dd5466da174ea0958be2dd575d5451383bc0b794
kujawiak <kuj4@o2.pl>
2019-01-25 10:35:15 +0100

    Edit storage item functionality

--- RCLAdmin.Core/Controllers/StorageController.cs
+++ RCLAdmin.Core/Controllers/StorageController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using RCLAdmin.Core.Data;
+using RCLAdmin.Core.Models;
 
 namespace RCLAdmin.Core.Controllers
 {
@@ -26,6 +27,57 @@ namespace RCLAdmin.Core.Controllers
                 .ToListAsync();
             var x = await accessories;
             return View(x);
+        }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var accessory = await _context
+                .PrinterAccessories
+                .SingleOrDefaultAsync(m => m.PrinterAccessoryId == id);
+            if (accessory == null)
+            {
+                return NotFound();
+            }
+
+            return View(accessory);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, PrinterAccessory accessory)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(accessory);
+                    //TODO: Notify registry about this update
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AccessoryExists(accessory.PrinterAccessoryId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool AccessoryExists(int id)
+        {
+            return _context.PrinterAccessories.Any(e => e.PrinterAccessoryId == id);
         }
     }
 }
