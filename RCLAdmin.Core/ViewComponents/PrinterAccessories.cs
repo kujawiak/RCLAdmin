faf41f7abd1498f0ca2e99cc377dd803d705bd8f
kujawiak <kuj4@o2.pl>
2019-03-06 12:19:17 +0100

    Added preview for available accessories in Pritner Details view.

---
+++ RCLAdmin.Core/ViewComponents/PrinterAccessories.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RCLAdmin.Core.Data;
+using RCLAdmin.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RCLAdmin.Core.ViewComponents
+{
+    public class PrinterAccessories : ViewComponent
+    {
+        private readonly RCLAdminContext _context;
+
+        public PrinterAccessories(RCLAdminContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(int printerTypeId)
+        {
+            var items = await GetItemsAsync(printerTypeId);
+            return View(items);
+        }
+
+        private Task<List<PrinterAccessory>> GetItemsAsync(int printerTypeId)
+        {
+            //return
+            var result = _context.PrinterAccessories
+            .Where(a => a.PrinterAccessoryType.Where(b => b.PrinterType.PrinterTypeId == printerTypeId).Any());
+                
+            return result.ToListAsync();
+        }
+    }
+}
